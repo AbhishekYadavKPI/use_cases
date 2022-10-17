df0dc4d290845293090c203e5bdc34070aae7e91
AbhishekYadavKPI <abhishek.yadav@kpipartners.com>
2022-10-17 18:06:13 +0530

    Renaming the controller file

--- CRUD/src/main/java/com/example/CRUD/StudentController.java
+++ CRUD/src/main/java/com/example/CRUD/StudentController.java
@@ -6,7 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 @RestController
-public class Controller2 {
+public class StudentController {
 	@Autowired
 	private StudentRepo er;//up to date
 	@GetMapping("/student") 
