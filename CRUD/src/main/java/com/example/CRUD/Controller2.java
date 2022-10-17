88f35eb792ad3a8ef6e376422ef99b282232f732
AbhishekYadavKPI <abhishek.yadav@kpipartners.com>
2022-10-17 17:44:55 +0530

    Commit for " CRUD code"

---
+++ CRUD/src/main/java/com/example/CRUD/Controller2.java
@@ -0,0 +1,32 @@
+package com.example.CRUD;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+@RestController
+public class Controller2 {
+	@Autowired
+	private StudentRepo er;//up to date
+	@GetMapping("/student") 
+	public List<StudentEntity> getSample(){
+		ArrayList<StudentEntity> list=new ArrayList<StudentEntity>();
+    	for(StudentEntity emt:er.findAll()) {
+    		StudentEntity temp=new StudentEntity();
+    		
+    		if(emt.getStudent_name().startsWith("M")) {
+    			temp.setId(emt.getId());
+    			temp.setStudent_name(emt.getStudent_name());
+    			list.add(temp);
+    		}
+    		
+    	}
+    	return list;
+	}
+		
+
+
+
+
+}
---
+++ CRUD/src/main/java/com/example/CRUD/StudentEntity.java
@@ -0,0 +1,34 @@
+package com.example.CRUD;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity 
+@Table(name="student")
+public class StudentEntity {
+
+  @Id
+  @GeneratedValue(strategy=GenerationType.AUTO)
+  private Integer id;
+  public Integer getId() {
+	return id;
+}
+public void setId(Integer id) {
+	this.id = id;
+}
+public String getStudent_name() {
+	return student_name;
+}
+public void setStudent_name(String student_name) {
+	this.student_name = student_name;
+}
+private String student_name;
+
+
+  
+
+
+}
